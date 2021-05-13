7995e49f5f053d31bb2a3600e1ec2ee2488e31bd
Acezz4120 <arthurandersennn@gmail.com>
2021-05-13 15:19:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/phptrial/SignUp.java
@@ -0,0 +1,94 @@
+package com.example.phptrial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class SignUp extends AppCompatActivity {
+
+    Button buttonSignUp;
+    TextView loginText;
+    TextInputEditText textInputEditTextFullName, textInputEditTextEmail, textInputEditTextUsername, textInputEditTextPassword;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        textInputEditTextFullName= findViewById(R.id.fullname);
+        textInputEditTextEmail= findViewById(R.id.email);
+        textInputEditTextUsername= findViewById(R.id.username);
+        textInputEditTextPassword= findViewById(R.id.password);
+        buttonSignUp= findViewById(R.id.buttonSignUp);
+        loginText= findViewById(R.id.loginText);
+        progressBar=findViewById(R.id.progress);
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String fullname, username, password, email;
+                fullname = String.valueOf(textInputEditTextFullName.getText());
+                username = String.valueOf(textInputEditTextUsername.getText());
+                password = String.valueOf(textInputEditTextPassword.getText());
+                email = String.valueOf(textInputEditTextEmail.getText());
+
+                if (!fullname.equals("") && !username.equals("") && !password.equals("") && !email.equals("")) {
+
+
+                    progressBar.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String[] field = new String[4];
+                            field[0] = "fullname";
+                            field[1] = "username";
+                            field[2] = "email";
+                            field[3] = "password";
+                            //Creating array for data
+                            String[] data = new String[4];
+                            data[0] = fullname;
+                            data[1] = username;
+                            data[2] = email;
+                            data[3] = password;
+
+                            PutData putData = new PutData("hhttp://127.0.0.1//LoginRegister/signup.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    if (result.equals("Sign up Success")){
+                                        Toast.makeText(getApplicationContext(), result,Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(),Login.class);
+                                        startActivity(intent);
+                                        finish();
+
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(), result,Toast.LENGTH_SHORT).show();
+
+                                    }
+                                    Log.i("PutData", result);
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(), " All fields are required", Toast.LENGTH_SHORT).show();
+                }
+            }
+    });
+    }
+}
